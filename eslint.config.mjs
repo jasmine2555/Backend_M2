eb06bdc9c0527e02cfd77ac54108009b7c96a7fd
Jasmine <jasminekaur9@academic.rrc.ca>
2026-05-26 20:18:29 -0500

    exclude test directory from ESLint strict type checking

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ export default tseslint.config(
             "**/dist/*",
             "**coverage/*",
             "**.github/*",
+            "**/test/*",
             "eslint.config.mjs",
             "jest.config.ts",
         ],
